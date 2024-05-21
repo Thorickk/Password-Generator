80b712fc09abafbcf50e98aad705cc794381b015
Thorickk <michnikiewicz@o2.pl>
2024-05-21 22:41:24 +0200

    update Header component and add styles for it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,9 @@ import styles from './Header.module.css'
 export function Header() {
 	return (
 		<header className={styles.header}>
-			<h1 className={styles.title}>Password Generator</h1>
-			<i className={`${styles.icon} fa-solid fa-key`}></i>
+			<h1 className={styles.title}>
+				Password Generator<i className={`${styles.icon} fa-solid fa-key`}></i>
+			</h1>
 		</header>
 	)
 }
